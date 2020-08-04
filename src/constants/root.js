f42c50bd9e6df4b23a4cd52c8a5451b7a643b2b6
anton-sergeenkov <warnes23@yandex.ru>
2020-08-05 00:51:48 +0300

    starter

--- src/constants/root.js
+++
@@ -1,2 +0,0 @@
-export const ROOT_INDEX = document.getElementById('root');
-export const ROOT_MODAL = document.getElementById('modal');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import 'regenerator-runtime/runtime'
-import App from './components/App';
-import Comics from './components/Comics';
-
-(async () => {
-	await App.render();
-	Comics.eventListener();
-})();
+console.log('Hello');
