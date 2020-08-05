3767e0e1eeb81b66d814953e98e750d0bb77ddd8
anton-sergeenkov <warnes23@yandex.ru>
2020-08-05 23:08:33 +0300

    starter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import 'regenerator-runtime/runtime'
-import App from './components/App';
-import Comics from './components/Comics';
+import 'regenerator-runtime/runtime';
+
+import { API_URL, URL_COMICS } from './constants/api';
+import { getDataApi } from './utils/getDataApi';
 
 (async () => {
-	await App.render();
-	Comics.eventListener();
+    const data = await getDataApi.getData(API_URL + URL_COMICS);
+    console.log(data);
 })();
