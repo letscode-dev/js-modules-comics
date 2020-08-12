0085cab4e3dc6885271c11df85656693fe822531
anton-sergeenkov <warnes23@yandex.ru>
2020-08-12 20:47:35 +0300

    starter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
-import Comics from '../Comics';
+import { API_URL, URL_COMICS } from '../../constants/api';
+import { getDataApi } from '../../utils/getDataApi';
+
 import './App.css';
 
 class App {
-	async render() {
-		await Comics.render();
-	}
+    async render() {
+        const data = await getDataApi.getData(API_URL + URL_COMICS);
+        console.log(data);
+    }
 }
 
 export default new App();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime';
 import App from './components/App';
-import Comics from './components/Comics';
 
 (async () => {
-	await App.render();
-	Comics.eventListener();
+    await App.render();
 })();
