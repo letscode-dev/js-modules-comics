c90418b513507883802308245892ab0e18684165
anton-sergeenkov <warnes23@yandex.ru>
2020-09-06 18:10:34 +0300

    starter

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -1,55 +1,48 @@
-import { getDataApi } from '../../utils/getDataApi';
 import { API_URL, URL_COMICS, URL_CHARACTERS, IMG_STANDARD_XLARGE, IMG_NOT_AVAILABLE } from '../../constants/api';
+import { getDataApi } from '../../utils/getDataApi';
 import { ROOT_INDEX } from '../../constants/root';
-
-import Characters from '../Characters';
-import Error from '../Error';
 
 import classes from './Comics.css';
 
 class Comics {
-	renderComics(data) {
-		let htmlContent = '';
+    async render() {
+        const data = await getDataApi.getData(API_URL + URL_COMICS);
 
-		data.forEach(({ id, title, thumbnail: { path, extension } }) => {
+        let htmlContent = '';
 
-			if (path.lastIndexOf(IMG_NOT_AVAILABLE) === -1) {
-				const imgSrc = path + '/' + IMG_STANDARD_XLARGE + '.' + extension;
-				const uri = API_URL + URL_COMICS + '/' + id + '/' + URL_CHARACTERS;
+        data.forEach(({ id, title, thumbnail: { path, extension } }) => {
+            
+            if (path.lastIndexOf(IMG_NOT_AVAILABLE) === -1) {
+                const uri = API_URL + URL_COMICS + '/' + id + '/' + URL_CHARACTERS;
+                const imgSrc = path + '/' + IMG_STANDARD_XLARGE + '.' + extension;
 
-				htmlContent += `
-					<li class="comics__item ${classes.comics__item}" data-uri="${uri}">
-						<span class="${classes.comics__name}">${title}</span>
-						<img class="img-contain ${classes.comics__img}" src="${imgSrc}" />
-					</li>
-				`;
-			}
-		});
+                htmlContent += `
+                    <li class="comics__item ${classes.comics__item}" data-uri="${uri}">
+                        <span class="${classes.comics__name}">${title}</span>
+                        <img class="img-contain ${classes.comics__img}" src="${imgSrc}" />
+                    </li>
+                `;
+            }
+        });
 
-		const htmlWrapper = `
-			<ul class="${classes.comics__container}">
-				${htmlContent}
-			</ul>
-		`;
+        const htmlWrapper = `
+            <ul class="${classes.comics__container}">
+                ${htmlContent}
+            </ul>
+        `;
+    
+        ROOT_INDEX.innerHTML = htmlWrapper;
+    }
 
-		ROOT_INDEX.innerHTML = htmlWrapper;
-	}
+    eventListener() {
+        document.querySelectorAll('.comics__item').forEach(element => {
+            const uri = element.getAttribute('data-uri');
 
-	async render() {
-		const data = await getDataApi.getData(API_URL + URL_COMICS);
-
-		data ? this.renderComics(data) : Error.render();
-	}
-
-	eventListener() {
-		document.querySelectorAll('.comics__item').forEach(element => {
-			const uri = element.getAttribute('data-uri');
-
-			element.addEventListener('click', () => {
-				Characters.render(uri);
-			})
-		})
-	}
+            element.addEventListener('click', () => {
+                console.log(uri);
+            })
+        })
+    }
 }
 
 export default new Comics();
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,5 @@
-// https://developer.marvel.com/docs
-
 export const API_KEY = 'a5837db97d72016c81a7a776f4240db9';
+
 export const API_URL = 'https://gateway.marvel.com/v1/public/';
 
 export const URL_COMICS = 'comics';
